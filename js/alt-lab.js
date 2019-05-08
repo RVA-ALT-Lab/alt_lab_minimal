67045f55fcedb8234eaab4dd7745c7e561bba988
Tom Woodward <twwoodward@me.com>
2019-05-08 12:08:03 -0400

    Initial commit

---
+++ js/alt-lab.js
@@ -0,0 +1,37 @@
+//ALT LAB STUFF
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("wrapper-navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop+60;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+console.log(document.getElementsByTagName('article')[0].id)
+console.log(document.getElementsByTagName('article')[0].offsetHeight)
+
+const urlParams = new URLSearchParams(window.location.search);
+const show = urlParams.get('show');
+
+//components to hide or show
+const headerNav = document.getElementById('wrapper-navbar');
+const rightSidebar = document.getElementById('right-sidebar');
+const footer = document.getElementById('wrapper-footer');
+
+if (show === 'article') {
+	headerNav.classList.add('hidden')
+	rightSidebar.classList.add('hidden')
+	footer.classList.add('hidden')
+}
