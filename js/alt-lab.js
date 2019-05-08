a909e62d7755912bc81640f0c8fc0d5cb0627881
Tom Woodward <twwoodward@me.com>
2019-05-08 14:57:23 -0400

    good ol' px

--- js/alt-lab.js
+++ js/alt-lab.js
@@ -28,11 +28,10 @@ let main = document.getElementById('primary');
 let iframeHeight = main.clientHeight;
 let embedHolder = document.getElementById('lms-embed-code')
 
-const embedOne ="<iframe frameborder='0' scrolling='no' width='100%' height='"+iframeHeight+"' ";
+const embedOne ="<iframe frameborder='0' scrolling='no' width='100%' height='"+iframeHeight+"px' ";
 const currentURL = window.location.href;
 const embedTwo ="src='"+currentURL+"?show=article'></iframe><script async src='https://rampages.us/extras/js/set-iframe-height-parent-min.js'></script>";
 embedHolder.value = embedOne+embedTwo;
-console.log(height);
 
 
 //make stuff invisible
